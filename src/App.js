68904e3feddd4f99b8034b70af3125f173a44ac3
AnhTuan1407 <anhtuan2003147@gmail.com>
2024-03-11 11:36:06 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Login } from "./component/login/Login";
+import { toast, ToastContainer } from "react-toastify";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import ScrollReveal from "scrollreveal";
+import 'react-toastify/dist/ReactToastify.css';
+import { Home } from "./component/home/home";
+import { NotFound } from "./component/404-not-found/NotFound";
 function App() {
+  const navigate = useNavigate();
+  const roles = localStorage.getItem("roles");
+  const token = localStorage.getItem("token");
+  const [showHeaderFooter, setShowHeaderFooter] = useState(true);
+
+  const checkTokenExpiration = () => {
+    // if (!token) return false;
+    // const { exp } = JSON.parse(atob(token.split(".")[1]));
+    // return Date.now() < exp * 1000;
+  };
+
+  useEffect(() => {
+    if (checkTokenExpiration() && window.location.pathname === "/login") {
+      toast.warning("Bạn đã đăng nhập!", { autoClose: 1000 });
+      setTimeout(() => {
+        navigate('/');
+      }, 1500)
+    }
+  }, [roles]);
+  useEffect(() => {
+    const sr = ScrollReveal({
+      distance: '60px',
+      duration: 2500,
+      delay: 400,
+    });
+
+    sr.reveal(`.footer .row`, {
+      origin: 'left',
+      interval: 100,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        {
+          !roles ? (
+            ""
+          ) : roles.includes('ROLE_ADMIN') ? (
+            <>
+              <Route path="/" element={<Home />}></Route>
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<NotFound />}></Route>
+            </>
+          )
+        }
+      </Routes>
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App;
