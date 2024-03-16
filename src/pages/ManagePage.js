4db95be35fc154f749ea5acc8a2a547ca3b6e130
Lê Hoài Anh <lehoaianh@MacBook-Pro-cua-Le.local>
2024-03-16 21:43:22 +0700

    hello

---
+++ src/pages/ManagePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../css/pages/ManagePage.css"
+
+const ManagePage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ManagePage;
