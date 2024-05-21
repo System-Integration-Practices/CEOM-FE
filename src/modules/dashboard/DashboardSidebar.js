5eb2647002a44a448a273831a9b31f6f3d800eda
Kienroro2003 <kienroro281@gmail.com>
2024-05-21 08:26:08 +0700

    update authorzation

--- src/modules/dashboard/DashboardSidebar.js
+++ src/modules/dashboard/DashboardSidebar.js
@@ -8,7 +8,7 @@ import {
   IconWithdraw,
 } from "components/icons";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { authLogOut } from "store/auth/auth-slice";
 const sidebarLinks = [
@@ -16,40 +16,48 @@ const sidebarLinks = [
     icon: <IconDashboard></IconDashboard>,
     title: "Dashboard",
     url: "/",
+    role: ["HR", "PAYROLL", "ADMIN"],
   },
   {
     icon: <IconCampaign></IconCampaign>,
     title: "Campaign",
     url: "/campaign",
+    role: ["ADMIN"],
   },
   {
     icon: <IconPayment></IconPayment>,
     title: "Payment",
     url: "/payment",
+    role: ["HR", "PAYROLL", "ADMIN"],
   },
   {
     icon: <IconWithdraw></IconWithdraw>,
     title: "Withdraw",
     url: "/withdraw",
+    role: ["PAYROLL"],
   },
   {
     icon: <IconProfile></IconProfile>,
     title: "Profile",
     url: "/profile",
+    role: ["HR"],
   },
   {
     icon: <IconLogout></IconLogout>,
     title: "Logout",
     url: "/logout",
+    role: ["HR", "PAYROLL", "ADMIN"],
   },
   {
     icon: <IconDarkmode></IconDarkmode>,
     title: "Light/Dark",
     url: "/#",
     onClick: () => {},
+    role: ["HR", "PAYROLL", "ADMIN"],
   },
 ];
 const DashboardSidebar = () => {
+  const { user } = useSelector((state) => state.auth);
   const navlinkClass =
     "flex items-center gap-x-5 md:w-12 md:h-12 md:justify-center md:rounded-lg md:mb-8  last:mt-auto last:bg-white last:shadow-sdprimary";
   const dispatch = useDispatch();
@@ -73,6 +81,7 @@ const DashboardSidebar = () => {
             </button>
           );
         }
+        if (user && !link.role.includes(user?.role?.name)) return <></>;
         return (
           <NavLink
             to={link.url}
